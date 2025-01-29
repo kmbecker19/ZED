2f2923d95cfb4a3cd318d18c2bf35ceea04313e0
Kyle Becker <kmb08625@uga.edu>
2025-01-29 13:54:08 -0500

    Add camera settings dialog to Dialogs with resolution

--- GUI/Dialogs.py
+++ GUI/Dialogs.py
@@ -1,10 +1,10 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QWidget, QHBoxLayout, QComboBox, QDialog, QDialogButtonBox, QToolBar, QStatusBar, QSlider
 from PySide6.QtGui import QPixmap, QAction, QMouseEvent, QPainter, QPen
-from PySide6.QtCore import Qt, QRect, QPoint, QSize
-
+from PySide6.QtCore import Qt, QRect, QPoint, QSize, Signal
+import pyzed.sl as sl
 
 class MessageDialog(QDialog):
-    def __init__(self, message: str):
+    def __init__(self, title: str, message: str):
         super().__init__()
         text = message
         self.setWindowTitle(title)
@@ -21,4 +21,52 @@ class MessageDialog(QDialog):
 
 class ImageSavedDialog(MessageDialog):
     def __init__(self):
-        super().__init__(f"Image saved!")
+        super().__init__("Image Saved", f"Image saved!")
+
+
+class CameraSettingsDialog(QDialog):
+    # Signal
+    settings_changed = Signal(sl.InitParameters)
+    def __init__(self, init_params: sl.InitParameters):
+        super().__init__()
+        self.setWindowTitle("Camera Settings")
+        self.init_params = init_params
+
+        # Resolution
+        resolution_mapping = {
+            sl.RESOLUTION.HD2K: "2K",
+            sl.RESOLUTION.HD1080: "1080p",
+            sl.RESOLUTION.HD720: "720p",
+            sl.RESOLUTION.VGA: "VGA"
+        }
+        resolution_label = QLabel("Resolution:")
+        self.resolution_combo = QComboBox()
+        self.resolution_combo.addItems(["2K", "1080p", "720p", "VGA"])
+        self.resolution_combo.setCurrentText(resolution_mapping[init_params.camera_resolution])
+        
+        # FPS
+
+        # Depth Mode
+
+        # Coordinate units
+
+        # Minimum Distance
+
+        # Maximum Distance
+
+
+'''
+        # Resolution
+        resolution_label = QLabel("Resolution:")
+        self.resolution_combo = QComboBox()
+        self.resolution_combo.addItems(["2K", "1080p", "720p", "VGA"])
+        self.resolution_combo.setCurrentIndex(self.resolution_combo.findText(self.init_params.camera_resolution.name()))
+
+        # Depth mode
+        depth_mode_label = QLabel("Depth Mode:")
+        self.depth_mode_combo = QComboBox()
+        self.depth_mode_combo.addItems(["Ultra", "Medium", "Low", "Night"])
+        self.depth_mode_combo.setCurrentIndex(self.depth_mode_combo.findText(self.init_params.depth_mode.name()))
+
+        # Coordinate units
+'''
